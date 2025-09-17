b3defef562d61ed0123033919a5ac12dc734a536
koi-as <lightningblade.901@gmail.com>
2025-09-17 10:58:21 -0400

    Basic UI is beginning to take shape. Next step is aligning things and adding entry fields for user input.

--- turtinator.py
+++ turtinator.py
@@ -1,48 +1,38 @@
-# import tkinter as tk
-# from tkinter import *
+import tkinter as tk
+from tkinter import *
 import turtle as turt
 from turtle import *
 
-color('red')
-turt.forward(100)
-turt.left(120)
-color('orange')
-turt.forward(100)
-turt.left(120)
-color('yellow')
-turt.forward(200)
-turt.left(120)
-color('light green')
-turt.forward(100)
-turt.left(120)
-color('green')
-turt.forward(200)
-turt.left(120)
-turt.forward(100)
-turt.left(120)
-color('light blue')
-turt.forward(200)
-turt.right(120)
-color('blue')
-turt.forward(100)
-turt.right(120)
-color('purple')
-turt.forward(200)
-turt.right(120)
-color('pink')
-turt.forward(100)
-turt.right(120)
-color('red')
-turt.forward(200)
-turt.right(120)
-color('orange')
-turt.forward(100)
-turt.right(120)
-color('yellow')
-turt.forward(100)
+win=tk.Tk()
+win.geometry('400x400')
+win.title('Draw Like A Turtle')
+win.config(bg='light blue')
 
-fillcolor('light blue')
+expl='''
+    This is supposed to be a simple program that takes your input in the box below and draws an image in a new window. There are a few buttons below for you to use as you need. The first starts the drawing, and you can watch as the turtle draws you a masterpiece! The second gives you an additional entry field for you to add a new command to. The third just closes this window... Good luck!
+    '''
 
-end_fill()
+command0=StringVar()
 
-turt.mainloop()
+def draw():
+    print('hello')
+
+def newBox():
+    print('oh no')
+# title label for app
+# explanation label
+# button to initiate turtle program
+# button to add function
+# button to end program
+# entry field to add function
+# some kind of looping function that pushes a new entry field whenever a user asks for one
+# I wonder what the limit could be?
+Label(win, text='Draw Like A Turty!').place(x=150, y=0)
+Label(win, text=expl, wraplength=window_width()).place(x=0, y=50)
+Button(win, text='DRAW', command=draw, bg='light green').place(x=0, y=100)
+Button(win, text='ADD BOX', command=newBox, bg='light blue').place(x=0, y=150)
+Button(win, text='LEAVE', command=win.destroy, bg='red').place(x=0, y=200)
+# Entry(win, textvariable=command0, relief=GROOFY)
+
+# turt.mainloop()
+win.mainloop()
